a2dbf66d124396927eff5cbfcaacd38af04b72f5
Arden Zhan <arden@protonmail.ch>
2017-11-27 21:00:11 -0600

    initial ruby commit

---
+++ arden_zhan/Fundamentals/intro.rb
@@ -0,0 +1,10 @@
+# JavaScript
+# var arr = ["Ruby", "Is", "Life"];
+# for(var i = 0; i < arr.length; i++) {
+#     console.log(arr[i]);
+# }
+
+arr = ["I", "code", "therefore", "I", "am"]
+for i in 0...arr.count
+    puts arr[i]
+end
