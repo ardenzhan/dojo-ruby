be9c8194ad81d394a04b893f6af62cfb5ebfcbfb
Arden Zhan <arden@protonmail.ch>
2017-11-30 13:15:14 -0600

    keyword arguments note

--- arden_zhan/Fundamentals/hashes.rb
+++ arden_zhan/Fundamentals/hashes.rb
@@ -6,3 +6,7 @@ puts h
 puts h.empty?
 puts h.has_key?(:first_name)
 puts h.has_value?("Coding")
+
+#Keyword Arguments
+# basically same as regular paramenters except default vars values are defined with : instead of =
+# cleaner way to use hash as argument
