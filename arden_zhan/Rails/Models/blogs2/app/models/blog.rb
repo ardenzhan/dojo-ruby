7ff1457659c18d7536256e8e29558fea410e14d3
Arden Zhan <arden@protonmail.ch>
2017-12-07 15:05:24 -0600

    Blogs/Posts/Messages II assignment

---
+++ arden_zhan/Rails/Models/blogs2/app/models/blog.rb
@@ -0,0 +1,12 @@
+class Blog < ActiveRecord::Base
+    has_many :owners
+    has_many :posts
+  
+    # all users that create/own blog
+    has_many :users, through: :owners
+  
+    # all users that posted on blog
+    has_many :user_posts, through: :posts, source: :user
+  
+    validates :name, :description, presence: true
+end
