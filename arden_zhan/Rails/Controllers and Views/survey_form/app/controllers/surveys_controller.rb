8211cc7a98fe874de5463386750214b8ed582e06
Arden Zhan <arden@protonmail.ch>
2017-12-11 21:19:04 -0600

    Survey Form assignment

---
+++ arden_zhan/Rails/Controllers and Views/survey_form/app/controllers/surveys_controller.rb
@@ -0,0 +1,19 @@
+class SurveysController < ApplicationController
+  def index
+    session[:views] ||= 0
+  end
+
+  def process_survey
+	session[:views] = session[:views] + 1
+
+	session[:result] = params[:survey]
+
+	flash[:success] = "Thanks for submitting this form! You have submitted this form #{ session[:views] } time(s) now."
+
+	redirect_to surveys_result_path
+  end
+
+  def result
+    @result = session[:result]
+  end
+end
