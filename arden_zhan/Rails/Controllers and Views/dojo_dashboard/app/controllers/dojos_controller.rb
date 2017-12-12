ffbbe16c3c92eb8d8164bc44056e1ff7249a8818
Arden Zhan <arden@protonmail.ch>
2017-12-12 11:45:17 -0600

    Dojo Dashboard II assignment

--- arden_zhan/Rails/Controllers and Views/dojo_dashboard/app/controllers/dojos_controller.rb
+++ arden_zhan/Rails/Controllers and Views/dojo_dashboard/app/controllers/dojos_controller.rb
@@ -2,4 +2,25 @@ class DojosController < ApplicationController
   def index
     @dojos = Dojo.all
   end
+
+  def new
+    @dojo = Dojo.new
+  end
+
+  def create
+    @dojo = Dojo.new(dojo_params)
+    if @dojo.save
+      # Controllers II section, for flash messages :notice
+      redirect_to root_url, notice: "Form submitted"
+    else
+      flash[:errors] = @dojo.errors.full_messages
+      redirect_to :back
+    end
+  end
+    
+  private
+    def dojo_params
+      params.require(:dojo).permit(:branch, :street, :city, :state)
+    end
+
 end
