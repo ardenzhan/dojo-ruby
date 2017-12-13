5fa672e36b27e1178c7c359719d0478ebcb0efc6
Arden Zhan <arden@protonmail.ch>
2017-12-12 21:14:03 -0600

    Method Testing assignment

--- arden_zhan/TDD/Ruby TDD/project/project.rb
+++ arden_zhan/TDD/Ruby TDD/project/project.rb
@@ -4,13 +4,25 @@
 # Also create an instance method called elevator_pitch that prints out name of project and its description separated by comma
 
 class Project
-    attr_accessor :name, :description
-    def initialize(name, description)
+    attr_accessor :name, :description, :owner
+    attr_reader :tasks
+    def initialize(name, description, owner)
         @name = name
         @description = description
+        @owner = owner
+        @tasks = []
     end
+
     def elevator_pitch
         "#{@name}, #{@description}"
+    end
+
+    def add_tasks task
+        @tasks << task
+    end
+
+    def print_tasks
+        @tasks.each { |task| puts task }
     end
 end
 
--- arden_zhan/TDD/Ruby TDD/project/project_spec.rb
+++ arden_zhan/TDD/Ruby TDD/project/project_spec.rb
@@ -1,10 +1,36 @@
-require_relative 'project' # include our Project class in our spec file
+require_relative 'project'
 RSpec.describe Project do  
 	before(:each) do 
-		@project1 = Project.new('Project 1', 'description 1') # create a new project and make sure we can set the name attribute
+		@project1 = Project.new('Project 1', 'description 1', 'John Doe')
+		@project2 = Project.new('Project 2', 'description 2', 'Jane Doe')
 	end
+
 	it 'has a getter and setter for name attribute' do
-		@project1.name = "Changed Name" # this line would fail if our class did not have a setter method
-		expect(@project1.name).to eq("Changed Name") # this line would fail if we did not have a getter or if it did not change the name successfully in the previous line.
+		@project1.name = "Changed Name"
+		expect(@project1.name).to eq("Changed Name")
 	end 
+
+	it 'has a getter and setter for owner attribute' do
+		@project1.owner = "Changed Owner"
+		expect(@project1.owner).to eq("Changed Owner")
+	end
+	
+	it 'has a method elevator_pitch to explain name and description' do
+		expect(@project1.elevator_pitch).to eq('Project 1, description 1')
+		expect(@project2.elevator_pitch).to eq('Project 2, description 2')
+	end
+
+	context 'methods for tasks' do
+		before(:each) do
+			1.upto(3) { |i| @project1.add_tasks("Task ##{i}") }
+		end
+
+		it 'has method add_tasks to push single task to @tasks and tasks method to return tasks' do
+			expect(@project1.tasks).to eq(['Task #1', 'Task #2', 'Task #3'])
+		end
+
+		it 'has method print_tasks that prints each task in project' do
+			expect{ @project1.print_tasks }.to output("Task #1\nTask #2\nTask #3\n").to_stdout
+		end
+	end
 end
