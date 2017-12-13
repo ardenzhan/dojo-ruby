3674cd169206d25209081e9517956ca55d2eb883
Arden Zhan <arden@protonmail.ch>
2017-12-12 20:30:21 -0600

    beginning TDD

---
+++ arden_zhan/TDD/Ruby TDD/project/project.rb
@@ -0,0 +1,19 @@
+# Assignment: Project
+
+# Create a Project Class that has the following attributes: name, description
+# Also create an instance method called elevator_pitch that prints out name of project and its description separated by comma
+
+class Project
+    attr_accessor :name, :description
+    def initialize(name, description)
+        @name = name
+        @description = description
+    end
+    def elevator_pitch
+        "#{@name}, #{@description}"
+    end
+end
+
+# project1 = Project.new("Project 1", "Description 1")
+# puts project1.name # => "Project 1"
+# puts project1.elevator_pitch  # => "Project 1, Description 1"
