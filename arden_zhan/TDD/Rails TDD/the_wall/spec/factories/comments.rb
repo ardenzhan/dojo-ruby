c26ac0a958802f8a44104fc3c8694f176959674f
Arden Zhan <arden@protonmail.ch>
2017-12-14 01:54:54 -0600

    The Wall assignment

---
+++ arden_zhan/TDD/Rails TDD/the_wall/spec/factories/comments.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :comment do
+    content "Comment that is long enough"
+    user nil
+    message nil
+  end
+end
---
+++ arden_zhan/TDD/Rails TDD/the_wall/spec/factories/messages.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :message do
+    message "Message that is long enough"
+    user nil
+  end
+end
