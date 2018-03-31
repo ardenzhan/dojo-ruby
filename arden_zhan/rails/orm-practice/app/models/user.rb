768d624fb85d2086eb80a6cf318e36d17a8e144a
Arden Zhan <arden@protonmail.ch>
2018-03-31 13:31:58 -0500

    removed orm-practice, can look at platform for review

--- arden_zhan/rails/orm-practice/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ActiveRecord::Base
-  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
-  validates :first_name, :last_name, length: { in: 2..20 }
-  validates :email, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
-end
