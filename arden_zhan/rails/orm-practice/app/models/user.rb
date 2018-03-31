df0c4c03090ffc36828fae79b473b393b83dd5d9
Arden Zhan <arden@protonmail.ch>
2018-03-31 13:28:53 -0500

    added validations to User model

--- arden_zhan/rails/orm-practice/app/models/user.rb
+++ arden_zhan/rails/orm-practice/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  validates :first_name, presence: true
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :first_name, :last_name, length: { in: 2..20 }
+  validates :email, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
 end
